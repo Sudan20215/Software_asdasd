86933a834f9b9f8cdffead476085046a93e54274
unknown <subinkc140@gmail.com>
2021-05-02 23:29:49 -0700

    heroku push

--- server.js
+++ server.js
@@ -26,13 +26,14 @@ mongoose.connect(
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // app.use("/api", require("./routes/api-routes.js"));
 // app.use("/", require("./routes/html-routes"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client"));
-});
+
 
 app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`));
